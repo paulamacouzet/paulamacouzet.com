7a0388ddcd392cab1b8a83fd2bd08177d39bda45
Paula Macouzet <paulamacouzet@gmail.com>
2026-09-16 21:28:33 +0700

    Fix Vite multipage build config and add image preloader

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        weddings: resolve(__dirname, 'weddings/index.html'),
+        gallery: resolve(__dirname, 'weddings/gallery/index.html'),
+        inquire: resolve(__dirname, 'weddings/inquire/index.html')
+      }
+    }
+  }
+});
